829e763adddb0bd8c2a7b5276d106455d9a35809
RaccoonCodes <61885457+RaccoonCodes@users.noreply.github.com>
2023-12-07 17:23:28 -0600

    Add project files.

---
+++ Money_ValueExchange/Website/Main_Page.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main_Page.aspx.cs" Inherits="Money_ValueExchange.Website.Main_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Money Exchange</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <link href="../Styles/site.css"  rel="stylesheet"/>
+    <link href ="../Content/bootstrap.min.css" rel ="stylesheet" />
+    <script src="../Scripts/bootstrap.min.js"></script>
+    <script src="../Scripts/jquery-3.7.0.min.js"></script>
+
+</head>
+
+<body class ="mainPage">
+    <form id="form1" runat="server">
+        <div class ="center">
+            <h1>Money Exchange</h1>
+            <p>Updated:12/7/2023</p>
+        </div>
+        <div class = "pad">
+            <asp:Label ID="MoneyIN1" runat="server" Text="I want to exchange"></asp:Label>
+            <asp:TextBox ID="InputCashID" runat="server"></asp:TextBox>
+            <asp:DropDownList ID="CurrencyID" runat="server"></asp:DropDownList>
+            <asp:Label ID="MoneyIN2" runat="server" Text="to"></asp:Label>
+            <asp:DropDownList ID="Currency2ID" runat="server"></asp:DropDownList>
+            <asp:Button runat="server" Text="Enter" CssClass="btn btn-primary" ID="BtnEnterID" OnClick="BtnEnterID_Click"></asp:Button>
+            <asp:RegularExpressionValidator ID="Reg_Expr_Input" runat="server" ErrorMessage="Please input valid value" ValidationExpression="^\d*(\.\d+)?$" ControlToValidate="InputCashID" ForeColor="Red"></asp:RegularExpressionValidator>
+            <p>&nbsp</p>
+            <asp:Label ID="ResultsID" runat="server" Text=""></asp:Label>
+            <p>&nbsp</p>
+            <asp:Label ID="MathWork" runat="server" Text=""></asp:Label>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Money_ValueExchange/Website/Main_Page.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Money_ValueExchange.Website
+{
+    public partial class Main_Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack){
+                CombineDropList(CurrencyID);
+                CombineDropList(Currency2ID);
+            }
+        }
+        private void CombineDropList(DropDownList addlist)
+        {
+            addlist.Items.Add(new ListItem("USD", "Dollar(s)"));
+            addlist.Items.Add(new ListItem("MXN", "Peso(s)"));
+            addlist.Items.Add(new ListItem("JPY", "Yen"));
+            addlist.Items.Add(new ListItem("AUD", "Australian Dollar(s)"));
+        }
+
+        protected void BtnEnterID_Click(object sender, EventArgs e)
+        {
+            string currency1 = CurrencyID.SelectedValue;
+            string currency2 = Currency2ID.SelectedValue;
+            string inputValue = InputCashID.Text;
+            string printresult = " ";
+            string formatedTotal = "";
+            string workShown = "";
+            
+           decimal total = 0;
+        
+            if(currency1 == currency2)
+            {
+                printresult = "Two currencies are the same, please select different currencies";
+                MathWork.Text = workShown;
+            }
+            else
+            {
+                total = Exchange(currency1, currency2,ref workShown);
+                formatedTotal = total.ToString("0.00");
+                printresult = $"Exchanging {inputValue} {currency1} to {currency2}";
+                MathWork.Text = $"{workShown} {formatedTotal} {currency2}";
+            }
+            
+            ResultsID.Text = printresult;
+        }
+
+        private decimal Exchange(string currencyText,string currencyText2, ref string mathWork)
+        {
+            decimal result = 0;
+            decimal currencyVal = decimal.Parse(InputCashID.Text); ;
+            decimal currencyVal2 = 0;
+
+            if((currencyText == "Dollar(s)" && currencyText2 == "Peso(s)") || (currencyText == "Peso(s)" && currencyText2 == "Dollar(s)"))
+            {
+                currencyVal2 = 17.30m; //1 USD = 17.30
+
+                if (currencyText == "Dollar(s)" && currencyText2 == "Peso(s)")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else
+                    result = formula(currencyVal, currencyVal2, false, ref mathWork);
+            }
+            else if ((currencyText == "Dollar(s)" && currencyText2 == "Yen") || (currencyText == "Yen" && currencyText2 == "Dollar(s)"))
+            {
+                currencyVal2 = 147.26m; //1 USD = 147.26
+
+                if(currencyText == "Dollar(s)" && currencyText2 == "Yen")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else
+                    result = formula(currencyVal, currencyVal2, false, ref mathWork);
+            }
+            else if((currencyText == "Dollar(s)" && currencyText2 == "Australian Dollar(s)")|| (currencyText == "Australian Dollar(s)" && currencyText2 == "Dollar(s)"))
+            {
+                currencyVal2 = 1.51m;//1USD = 1.51
+
+                if (currencyText == "Dollar(s)" && currencyText2 == "Australian Dollar(s)")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else
+                    result = formula(currencyVal, currencyVal2,false, ref mathWork);
+
+            }
+
+            else if ((currencyText == "Peso(s)" && currencyText2 =="Yen")|| (currencyText =="Yen" && currencyText2 == "Peso(s)"))
+            {
+                currencyVal2 = 8.20m; //1 Peso = 8.20 Yen
+
+                if (currencyText == "Peso(s)" && currencyText2 == "Yen")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else
+                    result = formula(currencyVal, currencyVal2, false, ref mathWork);
+
+            }
+
+            else if ((currencyText == "Australian Dollar(s)" && currencyText2 == "Yen") || (currencyText == "Yen" && currencyText2 == "Australian Dollar(s)"))
+            {
+                currencyVal2 = 95.17m; //1 AUD = 95.17
+                                       
+                if(currencyText == "Australian Dollar(s)" && currencyText2 == "Yen")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else 
+                    result = formula(currencyVal, currencyVal2,false,ref mathWork);
+                
+
+            }
+            else if ((currencyText == "Australian Dollar(s)" && currencyText2 == "Peso(s)") || (currencyText == "Peso(s)" && currencyText2 == "Australian Dollar(s)"))
+            {
+                currencyVal2 = 11.53m; //1 AUD = 11.53 Pesos
+
+                if (currencyText == "Australian Dollar(s)" && currencyText2 == "Peso(s)")
+                    result = formula(currencyVal, currencyVal2, true, ref mathWork);
+                else
+                    result = formula(currencyVal, currencyVal2, false, ref mathWork);
+            }
+
+                return result;
+        }
+        private decimal formula(decimal val1,decimal val2, bool flag, ref string mathout)
+        {
+
+            if (flag){
+                mathout = $"{val1} * {val2} = ";
+                return val1 * val2;
+            }
+            else {
+                mathout = $"{val1} / {val2} = ";
+                return val1 / val2; 
+            }
+            
+        }
+    }
+}
